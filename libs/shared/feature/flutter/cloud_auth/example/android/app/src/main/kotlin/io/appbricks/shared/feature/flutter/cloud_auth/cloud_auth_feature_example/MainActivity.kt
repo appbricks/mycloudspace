4728a8253f7f901018150572091ff577059ac79f
Mevan Samaratunga <mevansam@gmail.com>
2024-02-19 23:18:14 -0500

    remove example projects within plugin projects

--- libs/shared/feature/flutter/cloud_auth/example/android/app/src/main/kotlin/io/appbricks/shared/feature/flutter/cloud_auth/cloud_auth_feature_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.appbricks.shared.feature.flutter.cloud_auth.cloud_auth_feature_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- libs/shared/feature/flutter/cloud_profile/example/android/app/src/main/kotlin/io/appbricks/shared/feature/flutter/cloud_profile/cloud_profile_feature_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.appbricks.shared.feature.flutter.cloud_profile.cloud_profile_feature_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
