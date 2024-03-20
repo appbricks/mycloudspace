ec831667cf54d33ec7f7942d3c47fd57e491aa37
Mevan Samaratunga <mevansam@gmail.com>
2024-03-19 22:05:40 -0400

    free resource only once although dispose may be called multiple times.

--- libs/commons/dart/ffi_helper/lib/foreign_instance_stub.dart
+++ libs/commons/dart/ffi_helper/lib/foreign_instance_stub.dart
@@ -2,12 +2,12 @@ import 'dart:ffi' as ffi;
 
 import 'package:async_helper_ab/async_helper.dart';
 
-// ForeignInstanceStub is an abstract class that
-// is implemented by stubs for foreign classes.
-// It provides convenience methods for managing
-// cross-references with instances created in the
-// foreign language environment as well as boiler
-// plate code for dart ffi bookeeping.
+/// ForeignInstanceStub is an abstract class that
+/// is implemented by stubs for foreign classes.
+/// It provides convenience methods for managing
+/// cross-references with instances created in the
+/// foreign language environment as well as boiler
+/// plate code for dart ffi bookeeping.
 abstract class ForeignInstanceStub {
   // The handle to the foreign instance.
   ffi.Pointer<ffi.Void> get handle => _handle;
@@ -28,6 +28,13 @@ abstract class ForeignInstanceStub {
   static final Finalizer<_FinalizeWrapper> _finalizer =
       Finalizer((w) => w.finalize());
 
+  // Flag to indicate if the instance has
+  // been disposed.
+  bool _disposed = false;
+
+  /// Constructor for the ForeignInstanceStub takes the handle
+  /// to the foreign instance along with the foreign finalizer
+  /// function that frees the foreign instance.
   ForeignInstanceStub(ffi.Pointer<ffi.Void> handle,
       void Function(ffi.Pointer<ffi.Void>) foreignFinalizer) {
     if (handle == ffi.nullptr) {
@@ -39,9 +46,16 @@ abstract class ForeignInstanceStub {
   }
 
   void dispose() {
+    if (_disposed) {
+      return;
+    }
+    // If the instance has not been disposed then
+    // dispose the async runner and call the foreign
+    // finalizer to free the foreign instance.
     asyncRunner.dispose();
     _foreignFinalizer(_handle);
     _finalizer.detach(this);
+    _disposed = true;
   }
 }
 
@@ -57,9 +71,9 @@ class _FinalizeWrapper {
   }
 }
 
-// InstanceCreateError is thrown if the foreign
-// instance handle is null when the stub is
-// instantiated.
+/// InstanceCreateError is thrown if the foreign
+/// instance handle is null when the stub is
+/// instantiated.
 class InstanceCreateError extends Error {
   InstanceCreateError(this.message);
 
